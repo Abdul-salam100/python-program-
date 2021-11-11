2786b5cdee68306bb544890609074bfd2462b5dd
Abdul-salam100 <90543991+Abdul-salam100@users.noreply.github.com>
2021-11-11 22:22:59 +0530

    Delete even & odd.py

--- even & odd.py
+++
@@ -1,7 +0,0 @@
-x=input("Bahar ka cheez khaoge  : ")
-college=input("Sabse kharaab cheez ka naam batao: ")
-
-if(college=="apple"): 
-    print("makki , Machar,  coakroch or in sab ka guu")
-else:
-    	print("5 goosh(*_*)")

425a66ceca9d5b68761a3e3a5f9ea93fb36a631a
Abdul-salam100 <90543991+Abdul-salam100@users.noreply.github.com>
2021-11-11 22:18:21 +0530

    Add files via upload

---
+++ even & odd.py
@@ -0,0 +1,7 @@
+x=input("Bahar ka cheez khaoge  : ")
+college=input("Sabse kharaab cheez ka naam batao: ")
+
+if(college=="apple"): 
+    print("makki , Machar,  coakroch or in sab ka guu")
+else:
+    	print("5 goosh(*_*)")
